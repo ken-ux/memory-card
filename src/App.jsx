d2f952f6ea6d40d859facdbdff404a07b30da4f2
Kenny Nguyen <sokenns@gmail.com>
2024-02-16 16:04:50 -0800

    Import Scoreboard and Card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 // import { useState } from "react";
+import Scoreboard from "./components/Scoreboard.jsx";
+import Card from "./components/Card.jsx";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
-      <h1>Start</h1>
+      <h1>Pokémon Memory Game</h1>
+      <Scoreboard />
+      <Card pokemonName="charmander" />
+      <Card pokemonName="bulbasaur" />
+      <Card pokemonName="squirtle" />
     </>
   );
 }
